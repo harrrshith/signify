1c86c667fe744d583e224f391f054b188b36fe0a
Harshith <harshith.kotian24@gmail.com>
2024-03-26 23:22:37 +0530

    Add animated button
    Learnt animatedVectorDrawable in order to implement this feature. Implemented in a way in which I thought is correct

--- app/src/main/java/com/harrrshith/signify/MainActivity.kt
+++ app/src/main/java/com/harrrshith/signify/MainActivity.kt
@@ -1,9 +1,10 @@
 package com.harrrshith.signify
 
+import android.graphics.drawable.AnimatedVectorDrawable
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.harrrshith.signify.databinding.ActivityMainBinding
@@ -12,6 +13,8 @@ import com.harrrshith.signify.util.setStatusBarColor
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var avdBtn: AnimatedVectorDrawable
+    private var isClicked: Boolean = false
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
@@ -26,5 +29,30 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        setView()
     }
+
+    private fun setView(){
+        avdBtn = binding.avdButton.setAnimateResource(R.drawable.animated_vector_button_from)
+        binding.avdButton.setOnClickListener {
+            isClicked = !isClicked
+            binding.avdButton.apply {
+                avdBtn = if(isClicked){
+                    binding.avdButton.setAnimateResource(R.drawable.animated_vector_button_from)
+                }else{
+                    binding.avdButton.setAnimateResource(R.drawable.animated_vector_button_to)
+                }
+            }
+            avdBtn.start()
+        }
+    }
+
+    private fun ImageButton.setAnimateResource(drawableId: Int): AnimatedVectorDrawable{
+        this.apply {
+            setBackgroundResource(drawableId)
+            return background as AnimatedVectorDrawable
+        }
+    }
+
+
 }
